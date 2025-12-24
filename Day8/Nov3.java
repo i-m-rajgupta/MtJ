2bfc3278488abd111184e8c5f7b9ec16f6148c7c
Raj <neeleshgupta32154@gmail.com>
2025-12-24 18:57:44 +0530

    Added new files

--- Day8/Nov3.java
+++ Day8/Nov3.java
@@ -6,7 +6,7 @@ public class Nov3 {
         while(top <= arr.length-1 && 0 <= right){
             int el = arr[top][right];
             if(target == el){
-                System.out.println("Target is found at index : ( "+top+","+right+" )" );
+                System.out.println("Target is found at index : ("+top+","+right+")" );
                 break;
             }else if(target <el){
                 right--;
@@ -33,6 +33,65 @@ public class Nov3 {
         }
         }
     
+        public static int[] kadane1d(int arr[]){
+            int currSum = arr[0];
+            int maxSum = arr[0];
+            int start =0,end =0;
+            int tempStart = 0;
+            for(int i=1;i<arr.length;i++){
+              if(arr[i]>arr[i]+currSum){
+                 tempStart = i;
+                 currSum += arr[i];
+              }else{
+                   currSum += arr[i];
+              }
+
+              if(maxSum < currSum){
+                maxSum = currSum;
+                start = tempStart;
+                end = i;
+              }
+            }
+            return new int[]{maxSum,start,end};
+        }
+
+        public static void printMatrix(int matrix[][],int top,int bottom,int left,int right){
+            System.out.println("Largest sum submatrix : ");
+            for(int i = top;i<=bottom;i++){
+                for(int j=left;j<= right;j++){
+                    System.out.print(matrix[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+        public static void kadane2d(int matrix[][]){
+            int finalMax = Integer.MIN_VALUE;
+            int finalleft =0,finalRight =0;
+            int finalTop = 0,finalBottom =0;
+
+            for(int left =0;left<matrix[0].length;left++){
+                int temp[] = new int[matrix.length]; 
+               for(int right = left;right<matrix[0].length;right++){
+                 for(int i =0;i<temp.length;i++){
+                    temp[i] += matrix[i][right];
+                 }
+                 int tempMax[] = kadane1d(temp);
+                 int max = tempMax[0];
+                 int top = tempMax[1];
+                 int bottom = tempMax[2];
+                 if(max > finalMax ){
+                    finalMax = max;
+                     finalleft = left;
+                     finalRight = right;
+                     finalTop = top;
+                     finalBottom = bottom;
+                 }
+               }
+            }
+
+            System.out.println("Largest sum of submatrix is : "+finalMax);
+            printMatrix(matrix,finalTop,finalBottom,finalleft,finalRight);
+        }
         public static void print(int matrix[][]){
            int top=0,bottom = matrix.length-1;
            int left =0,right = matrix[0].length-1;
@@ -59,6 +118,34 @@ public class Nov3 {
         }
         System.out.println();
         }
+
+        public static void snake(int arr[][]){
+            for(int col =0;col < arr[0].length;col++){
+                for(int row =0;row<arr.length;row++){
+               if(col % 2 == 0){
+                System.out.print(arr[row][col] +" ");
+               }else{
+                System.out.print(arr[arr.length-1-row][col]+" ");
+               }
+            }
+            }
+            System.out.println();
+        }
+
+        public static boolean isSymmetric(int arr[][]){
+            if(arr.length != arr[0].length){
+                return false;
+            }
+
+            for(int i =0;i<arr.length;i++){
+                for(int j=i+1;j<arr[0].length;j++){
+                    if(  arr[i][j] != arr[j][i]){
+                      return false;
+                    }
+                }
+            }
+            return true;
+        }
     public static void main(String[] args) {
         // Search in a Row-Wise and Column-Wise Sorted Matrix.
       
@@ -75,10 +162,30 @@ public class Nov3 {
      searchRow(matrix, 35);
 
     //  Largest Sum Submatrix (Kadane’s 2D).
+    int matrix5[][] = {
+        {-10  ,-20 , 30 , 40},
+        {15 , -25  ,35,  45},
+        {27  ,-29 , 37,  -48},
+        {-32  ,-33  ,39  ,-50}
+      };
+      kadane2d(matrix5);
 
     // Print Boundary Elements Only.
      print(matrix);
      print(matrix1);
      print(matrix2);
+
+    //  Snake Pattern (Column-wise).
+     snake(matrix);
+
+    // Check if Matrix is Symmetric.
+   int matrix4[][] ={
+    {2,-1,3},
+    {-1,5,9},
+    {3,9,6}
+   };
+
+   System.out.println("The matrix is symmetric : "+isSymmetric(matrix4));
+
     }
 }
